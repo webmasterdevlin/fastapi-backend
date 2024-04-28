32565349ee561fd1bab53c26232a528ab6484b7c
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-04-28 12:44:34 +0200

    Refactor imports, update routes, and add error handling in posts_router.py

--- src/app/main.py
+++ src/app/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI, APIRouter, Security
 from fastapi.middleware.cors import CORSMiddleware
 
 from .features.users import users_routes
-from .features.posts import posts_router
+from .features.posts import posts_routes
 from .helpers.config import settings
 
 log = logging.getLogger(__name__)
@@ -69,60 +69,19 @@ async def shutdown_event() -> None:
     print("Application shutdown")
 
 
-# TODO: Fix 401 Unauthorized error in the frontend
-@router.get("/", dependencies=[Security(azure_scheme, scopes=["user_impersonation"])])
-async def root() -> dict[str, str]:
-    log.info("Root endpoint")
-    return {"message": "Hello World"}
-
-
 @router.get(
     "/health", dependencies=[Security(azure_scheme, scopes=[USER_IMPERSONATION_SCOPE])]
 )
-async def get_health_status() -> dict[str, str]:
+def get_health_status() -> dict[str, str]:
     url = get_url()
     return {"status": "UP", "database_connection_url": url}
 
 
 @router.get("/hello/{name}")
-async def say_hello(name: str) -> dict[str, str]:
+def say_hello(name: str) -> dict[str, str]:
     return {"message": f"Hello {name}"}
 
 
 app.include_router(router)
-app.include_router(posts_router.router, prefix=prefix, tags=["posts"])
+app.include_router(posts_routes.router, prefix=prefix, tags=["posts"])
 app.include_router(users_routes.router, prefix=prefix, tags=["users"])
-
-# @app.on_event('startup')
-# async def load_config() -> None:
-#     """
-#     Load OpenID config on startup.
-#     """
-#     await azure_scheme.openid_config.load_config()
-
-
-# app.include_router(
-#     api_router_azure_auth,
-#     prefix=settings.API_V1_STR,
-#     dependencies=[Security(azure_scheme, scopes=['user_impersonation'])],
-# )
-# app.include_router(
-#     api_router_multi_auth,
-#     prefix=settings.API_V1_STR,
-#     # Dependencies specified on the API itself
-# )
-# app.include_router(
-#     api_router_graph,
-#     prefix=settings.API_V1_STR,
-#     # Dependencies specified on the API itself
-# )
-
-# if __name__ == '__main__':
-#     parser = ArgumentParser()
-#     parser.add_argument('--api', action='store_true')
-#     parser.add_argument('--reload', action='store_true')
-#     args = parser.parse_args()
-#     if args.api:
-#         uvicorn.run('main:app', reload=args.reload)
-#     else:
-#         raise ValueError('No valid combination of arguments provided.')
